e4396a44f6d02d0a168bcfc66982a42a07bcf561
Jamie Phillips <cphill11@gmail.com>
2016-10-23 11:59:55 -0400

    Finished the appunpack.

--- src/Cake.AppPackager/Unpack/AppUnpacker.cs
+++ src/Cake.AppPackager/Unpack/AppUnpacker.cs
@@ -1,12 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
 
-namespace Cake.AppPackager.Unpack
-{
-    class AppUnpacker
-    {
+namespace Cake.AppPackager.Unpack {
+    public sealed class AppUnpacker : AppPackagerTool<AppPackagerSettings> {
+        private readonly ICakeEnvironment _environment;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppUnpacker"/> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="processRunner">The process runner.</param>
+        /// <param name="tools">The tool locator.</param>
+        /// <param name="resolver">The App Packager tool resolver.</param>
+        public AppUnpacker(IFileSystem fileSystem,
+            ICakeEnvironment environment,
+            IProcessRunner processRunner,
+            IToolLocator tools,
+            IAppPackagerResolver resolver) : base(fileSystem, environment, processRunner, tools, resolver) {
+            _environment = environment;
+        }
+
+        /// <summary>
+        /// Unpacks an application package with teh same structure as installed package.
+        /// </summary>
+        /// <param name="inputPackageName">Input name of the application package.</param>
+        /// <param name="outputDirectory">Output directory to unpack the application.</param>
+        /// <param name="settings">The settings.</param>
+        public void Unpack(string inputPackageName, IDirectory outputDirectory, AppPackagerSettings settings) {
+            if (string.IsNullOrWhiteSpace(inputPackageName)) {
+                throw new ArgumentNullException(nameof(inputPackageName));
+            }
+            if (outputDirectory == null) {
+                throw new ArgumentNullException(nameof(outputDirectory));
+            }
+            if (settings == null) {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            Run(settings, GetArguments(inputPackageName, outputDirectory, settings));
+        }
+
+
+        private ProcessArgumentBuilder GetArguments(string inputPackageName, IDirectory outputDirectory, AppPackagerSettings settings) {
+            var builder = new ProcessArgumentBuilder();
+            builder.Append("pack");
+
+            builder.Append("/p");
+            builder.AppendQuoted(inputPackageName);
+
+            builder.Append("/d");
+            builder.AppendQuoted(outputDirectory.ToString());
+
+            AddSwitchArguments(builder, settings);
+            return builder;
+        }
+
+        private static void AddSwitchArguments(ProcessArgumentBuilder builder, AppPackagerSettings settings) {
+            builder.AppendSwitch(settings.Localized, "l");
+            builder.AppendSwitch(settings.OverwriteOutput, "o");
+            builder.AppendSwitch(settings.PreventOverwriteOutput, "no");
+            builder.AppendSwitch(settings.SkipSemanticValidation, "nv");
+            builder.AppendSwitch(settings.EnableVerboseLogging, "v");
+        }
     }
 }
