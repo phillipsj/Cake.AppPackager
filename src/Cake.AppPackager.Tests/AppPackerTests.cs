56ed91bab521f294c852f9d20371c5071ee4ad9d
Jamie Phillips <cphill11@gmail.com>
2016-11-13 16:18:34 -0500

    Adding unit tests for AppPacker

---
+++ src/Cake.AppPackager.Tests/AppPackerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Cake.AppPackager.Tests.Fixtures;
+using Cake.Core;
+using Cake.Testing;
+using Should;
+using Should.Core.Assertions;
+
+namespace Cake.AppPackager.Tests {
+    public sealed class AppPackerTests {
+        public void ShouldThrowIfAppPackerExecutableWasNotFound() {
+            // Given
+            var fixture = new AppPackerFixture() {OutputPackage = "test.appx", ContentDirectory = "content"};
+            fixture.GivenDefaultToolDoNotExist();
+
+            // When
+            var result = Record.Exception(() => fixture.Run());
+
+            // Then
+            result.ShouldBeType<CakeException>();
+            result.Message.ShouldEqual("App Packager: Could not locate executable.");
+        }
+
+        public void ShouldThrowIfOutputPackageIsNull() {
+            // Given
+            var fixture = new AppPackerFixture() {ContentDirectory = "content"};
+
+            // When
+            var result = Record.Exception(() => fixture.Run());
+
+            // Then
+            result.ShouldBeType<ArgumentNullException>().ParamName.ShouldEqual("outputPackage");
+        }
+
+        public void ShouldThrowIfContentDirectoryIsNull() {
+            // Given
+            var fixture = new AppPackerFixture() {OutputPackage = "test.appx"};
+
+            // When
+            var result = Record.Exception(() => fixture.Run());
+
+            // Then
+            result.ShouldBeType<ArgumentNullException>().ParamName.ShouldEqual("contentDirectory");
+        }
+    }
+}
